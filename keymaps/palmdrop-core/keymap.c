46af29287f12d257dda97471f85e7189ed5c0e85
palmdrop <anton@exlex.se>
2024-01-19 22:17:17 +0100

    disable sentence case if caps lock is enabled

--- keymaps/palmdrop-core/keymap.c
+++ keymaps/palmdrop-core/keymap.c
@@ -174,10 +174,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * `-----------------------------------------------------------------------------------'
   */
   [_RAISE] = LAYOUT_planck_grid(
-      _______,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8, KC_9,    KC_0,    SE_PLUS,
-      TO(_BASE), KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_4,    KC_5, KC_6,    KC_0,    KC_DOT,
-      _______,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_1,    KC_2, KC_3,    SE_MINS, _______,
-      _______,   _______, _______, CK_LLCK, _______, _______, _______, _______, KC_0, _______, _______, _______
+      _______,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,  KC_9,    KC_0,    SE_PLUS,
+      TO(_BASE), KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_4,    KC_5,  KC_6,    KC_0,    KC_DOT,
+      _______,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_1,    KC_2,  KC_3,    SE_MINS, _______,
+      _______,   _______, _______, CK_LLCK, _______, _______, _______, _______, KC_0,  _______, _______, _______
   ),
 
   /* Navigation
@@ -506,7 +506,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
 
   // sentence case feature
-  if (is_sentence_case_enabled && !process_sentence_case(keycode, record)) {
+  if (
+    is_sentence_case_enabled && 
+    !is_caps_enabled && // disable if caps is enabled
+    !process_sentence_case(keycode, record)
+  ) {
     return false;
   }
 
